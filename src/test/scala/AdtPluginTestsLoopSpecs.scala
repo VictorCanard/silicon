db80e19aff924c8af9f2e1a6dda31054c2c4f554
Victor_Canard <69410903+VictorCanard@users.noreply.github.com>
2024-12-11 12:25:02 +0100

    add test add pre inner cond typing

---
+++ src/test/scala/AdtPluginTestsLoopSpecs.scala
@@ -0,0 +1,29 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2011-2021 ETH Zurich.
+
+
+import viper.silicon.Silicon
+import viper.silver.reporter.NoopReporter
+
+
+class AdtPluginTestsLoopSpecs extends SiliconTests {
+
+  override val testDirectories: Seq[String] = Seq("adt") //change
+
+  override val silicon: Silicon = {
+    val reporter = NoopReporter
+    val debugInfo = ("startedBy" -> "viper.silicon.AdtPluginTests") :: Nil
+    new Silicon(reporter, debugInfo)
+  }
+
+  override def verifiers = List(silicon)
+
+  override def configureVerifiersFromConfigMap(configMap: Map[String, Any]): Unit = {
+    val newConfigMap = configMap.updated("silicon:plugin", "viper.silver.plugin.standard.adt.AdtPlugin") //also change
+    super.configureVerifiersFromConfigMap(newConfigMap)
+  }
+
+}
